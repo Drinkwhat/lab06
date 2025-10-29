a32ae222aec8feb1715ae6e87d636538ce83c5c4
Luca Bevilacqua <luca.bevilacqua6@studio.unibo.it>
2025-10-29 16:13:33 +0100

    Implement retry logic for network operations in UseArithmeticService

--- 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/UseArithmeticService.java
@@ -3,6 +3,7 @@ package it.unibo.exceptions;
 import it.unibo.exceptions.fakenetwork.api.NetworkComponent;
 import it.unibo.exceptions.fakenetwork.impl.ServiceBehindUnstableNetwork;
 
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static it.unibo.exceptions.arithmetic.ArithmeticService.DIVIDED;
@@ -48,6 +49,14 @@ public final class UseArithmeticService {
          * This method should re-try to send message to the provided server, catching all IOExceptions,
          * until it succeeds.
          */
+        while (true) {
+            try {
+                server.sendData(message);
+                break;
+            } catch (IOException e) {
+                LOG.println("Error in retrySendOnNetworkError: " + e);
+            }
+        }
     }
 
     private static String retryReceiveOnNetworkError(final NetworkComponent server) {
@@ -55,7 +64,16 @@ public final class UseArithmeticService {
          * This method should re-try to retrieve information from the provided server, catching all IOExceptions,
          * until it succeeds.
          */
-        return null;
+        String res;
+        while (true) {
+            try {
+                res = server.receiveResponse();
+                break;
+            } catch (IOException e) {
+                LOG.println("Error in retryReceiveOnNetworkError: " + e);
+            }
+        }
+        return res;
     }
 
     private static void assertEqualsAsDouble(final String expected, final String actual) {
