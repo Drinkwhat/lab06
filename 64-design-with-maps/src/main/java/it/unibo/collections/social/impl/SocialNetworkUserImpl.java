ee7bad9411ab8fb1bde287c84e063bde1852b5cc
Luca Bevilacqua <luca.bevilacqua6@studio.unibo.it>
2025-10-31 14:51:02 +0100

    Remove unused imports in SocialNetworkUserImpl class to clean up the code

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -9,12 +9,10 @@ import it.unibo.collections.social.api.User;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * This will be an implementation of
